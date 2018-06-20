997ec4643f39dfe76a4c9acc95742f3caff30d92
JunttilaJoonas <junttila.joonas@gmail.com>
2018-06-20 15:24:24 +0300

    Peli valmis?

--- src/Paavalikko.java
+++ src/Paavalikko.java
@@ -7,10 +7,35 @@ import java.util.Scanner;
 public class Paavalikko {
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
-
-
-
         JoonaksenPeli joonas = new JoonaksenPeli();
+        MiikanPeli miika = new MiikanPeli();
+        NikonPeli niko = new NikonPeli();
+
+
+        System.out.println("Tervetuloa pelaamaan! Kenen peliä haluaisit pelata? (Joonas, Miika tai Niko)");
+
+        outer:
+        while (true) {
+            String vastaus = lukija.nextLine().toLowerCase();
+            switch (vastaus) {
+                case "joonas":
+                    joonas.kaynnista();
+                    break outer;
+                case "miika":
+                    miika.kaynnista();
+                    break outer;
+                case "niko":
+                    niko.kaynnista();
+                    break outer;
+                default:
+                    System.out.println("Väärä nimi, yritä uudelleen!");
+            }
+        }
+
+
+
+
+
 
     }
 }
